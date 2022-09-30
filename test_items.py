912c2724f3137cd5530bbd6b19a437489ccfe6af
unknown <alexsandrvanin@gmail.com>
2022-09-30 13:15:46 +0300

    Added test_items file

---
+++ test_items.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+
+def test_locale(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    button = browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    assert button, 'expected: "button in page", actual: "button not page"'
