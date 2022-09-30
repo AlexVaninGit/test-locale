07900e587497b5c2550917bd5fdb27a8109a1dc0
unknown <alexsandrvanin@gmail.com>
2022-09-30 14:26:44 +0300

    Start test project

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='en',
                      help="locale for browser client")
 
 
